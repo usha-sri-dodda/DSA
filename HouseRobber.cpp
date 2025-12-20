de0f7526a5f292d243741a110ec9d3e9e669b788
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2025-12-20 10:35:35 +0530

    Maximum sum of non adjacent elements

---
+++ HouseRobber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev=nums[0],prev2=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int notTake=0+prev;
+            int curr=max(take,notTake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
