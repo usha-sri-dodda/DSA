961bed61fcc5ebb67fecafab9234223ac2e1e92b
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2025-12-20 10:36:39 +0530

    Update HouseRobber.cpp

--- HouseRobber.cpp
+++ HouseRobber.cpp
@@ -3,7 +3,6 @@ public:
     int rob(vector<int>& nums) {
         int prev=nums[0],prev2=0;
         int n=nums.size();
-        
         for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1) take+=prev2;
