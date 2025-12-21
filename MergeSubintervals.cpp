5eb1a8031b5ec6eb112af379a8eb77abda6d9260
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2025-12-21 15:53:07 +0530

    Update MergeSubintervals.cpp

--- MergeSubintervals.cpp
+++ MergeSubintervals.cpp
@@ -7,7 +7,6 @@ public:
         for(int i=0;i<n;i++){
             if(ans.empty()||arr[i][0]>ans.back()[1]){
                 ans.push_back(arr[i]);
-                
             }
             else{
                 ans.back()[1]=max(ans.back()[1],arr[i][1]);
