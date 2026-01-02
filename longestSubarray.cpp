31f68680bec2f852afd8ebd724944987a99ef25e
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-01-02 15:09:37 +0530

    update the longest subarray sum problem

--- longestSubarray.cpp
+++ longestSubarray.cpp
@@ -9,31 +9,25 @@ public:
         
         // To store the maximum length of the subarray
         int maxLen = 0;
-        
         // Pointers to mark the start and end of window
         int left = 0, right = 0;
-        
         // To store the sum of elements in the window
         int sum = nums[0];
         
         // Traverse all the elements
         while(right < n) {
-            
             // If the sum exceeds K, shrink the window
             while(left <= right && sum > k) {
                 sum -= nums[left];
                 left++;
             }
-            
             // store the maximum length
             if(sum == k) {
                 maxLen = max(maxLen, right - left + 1);
             }
-            
             right++;
             if(right < n) sum += nums[right];
         }
-        
         return maxLen;
     }
 };
