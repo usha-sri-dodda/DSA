3d5f00c914fc64ca36b71715123bddba63304e5b
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-01-03 13:16:22 +0530

    Add lowerBound and upperBound functions

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,3 +1,4 @@
+// Binary Search
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -12,5 +13,45 @@ public:
 
         }
         return -1;
+    }
+};
+
+// LowerBound
+class Solution {
+public:
+    int lowerBound(vector<int>& nums, int target) {
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target)   {
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+};
+
+// Upper Bound
+class Solution {
+public:
+    int upperBound(vector<int>& nums, int target) {
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>target)   {
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
     }
 };
