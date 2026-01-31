b708d3c88810959700485f999088cef4aea7a5cf
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-01-31 15:46:44 +0530

    Refactor Node class and clone linked list methods

--- copyListRandomPtr.cpp
+++ copyListRandomPtr.cpp
@@ -1,36 +1,30 @@
-
-#include <iostream>
-using namespace std;
-
-// Node class to represent
-// elements in the linked list
+/*
+// Definition for a Node.
 class Node {
 public:
-    // Data stored in the node
-    int data;           
-     // Pointer to the next node
-    Node *next;        
-    // Pointer to a random
-    // node in the list
-    Node *random;       
-
-    // Constructors for Node class
-    Node() : data(0), next(nullptr), random(nullptr){}; 
-    Node(int x) : data(x), next(nullptr), random(nullptr) {} 
-    // Constructor with data,
-    // next, and random pointers
-    Node(int x, Node *nextNode, Node *randomNode) :
-            data(x), next(nextNode), random(randomNode) {}  
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
 };
+*/
 
+class Solution {
+public:
+    
 // Function to insert a copy of each
 // node in between the original nodes
 void insertCopyInBetween(Node* head){
     Node* temp = head;
     while(temp != NULL){
         Node* nextElement = temp->next;
-        // Create a new node with the same data
-        Node* copy = new Node(temp->data);  
+        // Create a new node with the same val
+        Node* copy = new Node(temp->val);  
         
         // Point the copy's next to
         // the original node's next
@@ -97,11 +91,9 @@ Node* getDeepCopyList(Node* head){
     return dummyNode->next;   
 }
 
-// Function to clone the linked list
-Node *cloneLL(Node *head){
-    // If the original list
-    // is empty, return null
-    if(!head) return nullptr;   
+
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;   
     
      // Step 1: Insert copy of
      // nodes in between
@@ -111,48 +103,6 @@ Node *cloneLL(Node *head){
     connectRandomPointers(head);  
     // Step 3: Retrieve the deep
     // copy of the linked list
-    return getDeepCopyList(head); 
-}
-
-// Function to print the cloned linked list
-void printClonedLinkedList(Node *head) {
-    while (head != nullptr) {
-        cout << "Data: " << head->data;
-        if (head->random != nullptr) {
-            cout << ", Random: " << head->random->data;
-        } else {
-            cout << ", Random: nullptr";
-        }
-        cout << endl;
-         // Move to the next node
-        head = head->next;  
+    return getDeepCopyList(head);
     }
-}
-
-// Main function
-int main() {
-    // Example linked list: 7 -> 14 -> 21 -> 28
-    Node* head = new Node(7);
-    head->next = new Node(14);
-    head->next->next = new Node(21);
-    head->next->next->next = new Node(28);
-
-    // Assigning random pointers
-    head->random = head->next->next;
-    head->next->random = head;
-    head->next->next->random = head->next->next->next;
-    head->next->next->next->random = head->next;
-
-    cout << "Original Linked List with Random Pointers:" << endl;
-    printClonedLinkedList(head);
-
-    // Clone the linked list
-    Node* clonedList = cloneLL(head);
-
-    cout << "\nCloned Linked List with Random Pointers:" << endl;
-    printClonedLinkedList(clonedList);
-
-    return 0;
-}
-
-                                
+};
