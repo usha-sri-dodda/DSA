96f9463bc404c03f0c09ac80fdaae84f40a9e2c7
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-02-02 10:38:09 +0530

    Refactor power function for clarity and efficiency

--- pow(x,n).cpp
+++ pow(x,n).cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    double power(double x,long long n,double res){
-        if(n<1) return 1.0*res;
-        if(n%2==1)  res*=x,n=n-1;
-        else x=x*x,n=n/2;
-        return power(x,n,res);
+    double power(double x,long long n){
+        
+        if(n==0)    return 1.0;
+        if(n==1) return x;
+        if(n%2==1)  return x*power(x,n-1);
+        return power(x*x,n/2);
     }
     double myPow(double x, int n) {
-        double res=1;
         long long nn=n;
-        if(nn<0)    nn=-nn;
-        res=power(x,nn,res);
-        if(n<0) res=1.0/res;
-        return res;
+        
+        if(n<0) return  1.0/power(x,-nn);
+        return power(x,n);
     }
 };
