8b8d175828711d4b5d5e4124becbeda464370b41
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-02-13 10:27:26 +0530

    Update bitManupulationBasic.cpp

--- bitManupulationBasic.cpp
+++ bitManupulationBasic.cpp
@@ -55,3 +55,15 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor1=0;
+        for(int i=0;i<nums.size();i++){
+            xor1=xor1^nums[i];
+        }
+        return xor1;
+    }
+};
