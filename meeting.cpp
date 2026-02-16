af1e5a58db681cbbdf1c698f5861baeb4433d12c
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-02-16 10:36:25 +0530

    Create meeting.cpp

---
+++ meeting.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Code Here
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int i=1;i<n;i++){
+            if(arr[i][0]<arr[i-1][1])   return false;
+        }
+        return true;
+        
+    }
+};
