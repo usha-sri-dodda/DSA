4558569655dc11bef70c35ce3e16ec5ec37b30f0
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-02-23 17:07:06 +0530

    Implement dynamic programming solution for climbStairs
    Added a dynamic programming approach to solve the climbing stairs problem.

--- climbStairs.cpp
+++ climbStairs.cpp
@@ -9,8 +9,22 @@ public:
             prev = temp;
         }
         return curr;
-        // int a=climbStairs(n-1);
-        // int b=n-a;
-        // return a+b;
+      
+    }
+};
+
+//Approach 2
+class Solution {
+public:
+    int dp[46];
+    int solve(int n){
+        if(n<=2)  return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+        
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
