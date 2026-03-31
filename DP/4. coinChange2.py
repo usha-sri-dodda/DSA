18c8ff4190d84f69e0c9adc4cc9810d6fc772b24
Usha Sri <159525857+usha-sri-dodda@users.noreply.github.com>
2026-03-31 16:01:02 +0530

    Add coin change dynamic programming solution
    Implement dynamic programming solution for coin change problem.

---
+++ DP/4. coinChange2.py
@@ -0,0 +1,10 @@
+
+        dp = [0] * (amount + 1)
+
+        dp[0] = 1
+
+        for coin in coins:
+            for amt in range(coin, amount + 1):
+                dp[amt] += dp[amt - coin]
+
+        return dp[amount]
